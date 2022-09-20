944c2ccd0c3e3bc8258e52508010d9d630621da5
KaewSathita <kaew.gothai@gmail.com>
2022-09-20 15:55:57 +0700

    done login frontandbackend

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,3 +1,7 @@
 import axios from '../config/axios';
 
 export const register = input => axios.post('/auth/register', input)
+export const login = ({emailOrMobile, password} )=> axios.post('/auth/login', {emailOrMobile, password})
+
+
+// export const login = input => axios.post('/auth/login', input) 
