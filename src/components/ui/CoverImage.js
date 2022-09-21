2a98cd4762b32c2d1d123209dbff801272409cf5
KaewSathita <kaew.gothai@gmail.com>
2022-09-21 17:25:42 +0700

    getme done

---
+++ src/components/ui/CoverImage.js
@@ -0,0 +1,12 @@
+import coverImage from '../../assets/images/cover-image.png'
+function CoverImage({ src }) {
+  return (
+    <img
+          src={src || coverImage}
+          alt="cover"
+          className="img-fluid position-absolute top-50 start-50 translate-middle"
+        />
+  )
+}
+
+export default CoverImage
